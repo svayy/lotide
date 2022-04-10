a049cc90591e3d4b5d413c0346f867153162eb82
Raymond <102096183+twiggytwig@users.noreply.github.com>
2022-04-10 13:51:02 -0400

    Add files via upload

---
+++ countOnly.js
@@ -0,0 +1,14 @@
+const countOnly = function (allItems, itemsToCount) {
+    const resultobj = {};
+    for (const objectof allItems) {
+      if (itemsToCount[object]) {
+        if (resultobj[object]) { resultObj[object] += 1;
+        else resultObj[object] = 1;
+      }
+    }
+    console.log(resultObj);
+    return resultObj;
+  };
+
+
+    
