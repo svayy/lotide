9948ca586a1185fb6c1c2cf25c995aa7de996ed9
Raymond <102096183+twiggytwig@users.noreply.github.com>
2022-04-10 15:14:44 -0400

    Add files via upload

---
+++ countletters.js
@@ -0,0 +1,8 @@
+const letterscount = function(allItems) {
+    const letterscount = object;
+    for (const item of allItems) {
+        letterscount(letter[ = letterscount[letter] + 1 || 1;
+    }
+}  
+ return letterscount;
+
