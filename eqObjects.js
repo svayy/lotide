bcc43b8695e1cc00d8dc24934d9d76eacbaf9001
Raymond <102096183+twiggytwig@users.noreply.github.com>
2022-04-10 16:17:11 -0400

    Add files via upload

---
+++ eqObjects.js
@@ -0,0 +1,10 @@
+const eqObjects = function(object1, object2) {
+    const obj1 = Object.keys(object1).length;
+    const obj2 = Object.keys(object2).length;
+if (obj1 === obj2.length) {
+    return false;
+ } else {
+      if (obj1[key] !== obj2[key]) {}
+      return false;
+} 
+    return true;
