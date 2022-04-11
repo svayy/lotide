d1aba53cc4b2e03889ce945bb164b9770b5b1ffc
Raymond <102096183+twiggytwig@users.noreply.github.com>
2022-04-11 17:18:14 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,7 +1,4 @@
-const tail = function(array){
-    return array.slice(1);
-}
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const tail = function(array) {
+    return array.slice(1)
+};
+module.exports = tail;
