031e952c16933df782c1aa18b95b4db3a0d0bc79
Raymond <102096183+twiggytwig@users.noreply.github.com>
2022-04-12 08:49:39 -0400

    Add files via upload

---
+++ takeUntil.js
@@ -0,0 +1,7 @@
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
