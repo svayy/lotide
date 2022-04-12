1decc9f149b84308445d67590ef162bb643f344f
Raymond <102096183+twiggytwig@users.noreply.github.com>
2022-04-11 22:37:59 -0400

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,23 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const without = require('./without');
+const countLetters = require('./countLetters');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
+  eqObjects,
+  eqArrays,
+  without,
+  countLetters
+     
+};
